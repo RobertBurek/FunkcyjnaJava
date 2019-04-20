9ddd9837024a637464e22333d0fb98b4ac1bb25e
Robert Burek <robertburek@wp.pl>
2019-04-20 21:54:05 +0200

    STREAM - strumienie danych , prosta iteracja po liście
       jednokrotna iteracja streama jest trochę dziwna ale stream ma wiele
       metod do zaimplementowania

--- src/main/java/functional/App.java
+++ src/main/java/functional/App.java
@@ -1,8 +1,8 @@
 package functional;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.function.*;
+import java.util.stream.*;
 
 /**
  * Created by Robert Burek
@@ -15,14 +15,44 @@ public class App {
         Supplier<List<Student>> supplyPredefinedStudent = App::createData;
         Predicate<Student> over30 = student -> (student.getAge() > 30);
         Consumer<String> print = System.out::println;
+        Consumer<Integer> printWiek = System.out::println;
         Function<Student, String> getStudentName = Student::getName;
+        Function<Student, Integer> getStudentAge = Student::getAge;
         BiFunction<Student, String, Student> changeIndex = Student::changeIndexNumber;
-        System.out.println("Imiona studentów z wykorzystaniem Function: ");
+        System.out.println("Imiona studentów >30lat z wykorzystaniem STREAMa: ");
 
-        consumerStudentsNew(filterStudents(supplyPredefinedStudent, over30), getStudentName, print);
+//        consumerStudentsNew(filterStudents(supplyPredefinedStudent, over30), getStudentName, print);
 
+        List<Student> students = supplyPredefinedStudent.get();
+
+       // students.stream().filter(over30).map(getStudentName).forEach(print);
+//        powyższy stream jest do wykorzystania tylko raz, tylko raz można przeiterować,
+//         próba kolejnego użycia tego strumienia daje wyjątek IllegalStateException
+//           co poniżej udowniłem
+//        Stream<Student> studentStream= students.stream();
+//        studentStream.filter(over30).map(getStudentName).forEach(print);
+//        studentStream.forEach(student -> student.getAge());
+
+        // students.stream().filter(over30).map(getStudentName).forEach(print);
+        // zapis powyżej mógłby wyglądać w sposób tradycyjny tak jak poniżej
+//        List<Student> data = createData();
+//        for(Student s: data) {
+//            if(s.getAge()>30) {
+//                String name = s.getName();
+//                System.out.println(name);
+//            }
+//        }
+
+        students.stream().filter(over30).map(getStudentName).forEach(print);
 
         System.out.println("");
+
+        System.out.println("Imiona wszystkich studentów z wykorzystaniem STREAMa: ");
+        students.stream().map(getStudentName).forEach(print);
+        System.out.println("");
+        System.out.println("Wiek wszystkich studentów z wykorzystaniem STREAMa: ");
+        students.stream().map(getStudentAge).forEach(printWiek);
+
     }
 
     private static void consumerStudentsNew(List<Student> students, Function<Student, String> function, Consumer<String> consumer) {
