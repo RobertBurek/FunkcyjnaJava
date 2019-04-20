67e8992f343d6b70f9bdfe7a85a9f9509ecf0542
Robert Burek <robertburek@wp.pl>
2019-04-21 01:00:51 +0200

    FILTER - metoda streama , przepuszcza spełniakjące warunek wartości
       trzeba pamiętać, że nie odfiltrowuje wartości spełniających warunek
       tylko przepuszcza wartości spełniające warunek. Trochę odwrotnie.

--- src/main/java/functional/App.java
+++ src/main/java/functional/App.java
@@ -15,8 +15,21 @@ public class App {
         Consumer<String> print = System.out::println;
         Function<Student, String> getStudentName = Student::getName;
         System.out.println("Imiona wszystkich studentów: ");
-
         createDataStream().filter(over30).map(getStudentName).forEach(print);
+
+//     pełny zapis FILTER z implementacją metody test z interfejsu predicate
+//        createDataStream().filter(new Predicate<Student>() {
+//            @Override
+//            public boolean test(Student student) {
+//                return student.getName().equals("Robert");
+//            }
+//        }).forEach(System.out::println);
+
+        createDataStream().filter(student -> student.getName().equals("Robert")).forEach(System.out::println);
+
+//        można odfiltrować drugim filtrem
+        createDataStream().filter(student -> student.getName().equals("Robert")).filter(over30).forEach(System.out::println);
+
 
     }
 
