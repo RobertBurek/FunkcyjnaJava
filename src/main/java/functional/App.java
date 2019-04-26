f1443b6cf9ca00c5857f42b7cc357bdb1e8994a8
Robert Burek <robertburek@wp.pl>
2019-04-26 23:04:01 +0200

    findFirst, anyMatch, allMatch, noneMatch - kilka rozwiązań

--- src/main/java/functional/App.java
+++ src/main/java/functional/App.java
@@ -1,5 +1,6 @@
 package functional;
 
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -17,11 +18,42 @@ public class App {
         Consumer<Object> print = System.out::println;
         Function<Student, String> getStudentName = Student::getName;
 
-        System.out.println("Imiona wszystkich studentów alfabetycznie ");
+        System.out.println("Imiona wszystkich studentów alfabetycznie: ");
         createDataStream()
                 .map(getStudentName)
                 .sorted()
                 .forEach(print);
+
+        System.out.println("Pierwsze (findFirst) imię speniajace warunek: ");
+        Optional<String> first = createDataStream()
+                .filter(over30)
+                .findFirst()
+                .map(getStudentName);
+        System.out.println(first.get());
+
+        System.out.println("Pierwszy student w wieku 24: ");
+        System.out.println(createDataStream()
+                .filter(student -> student.getAge() == 24)
+                .findFirst().get().getName());
+
+        System.out.println("Pierwsze (findFirst) imię speniajace warunek: ");
+        createDataStream()
+                .filter(over30)
+                .findFirst()
+                .map(getStudentName);
+        System.out.println(first.get());
+
+        System.out.println("Prawda czy warunek spełniony czy istnieje Robert B  (anyMatch): ");
+        Predicate<String> isRobert = s -> s.equals("Robert B");
+        System.out.println(createDataStream()
+                .map(getStudentName)
+                .anyMatch(isRobert));
+
+        System.out.println("Prawda czy warunek spełniony dla wszystkich age > 20 (allMatch/noneMatch): ");
+        System.out.println(createDataStream()
+                .map(Student::getAge)
+                .allMatch(age -> age > 20));
+
     }
 
     private static Stream<Student> createDataStream() {
@@ -29,8 +61,8 @@ public class App {
         Student robert = new Student("Robert B", 24, "223344");
         Student monika = new Student("Monika", 36);
         Student robert1 = new Student("Robert C", 33, "555555");
-        Student ania = new Student("Anna", 35, "564534");
-        Student zenek = new Student("Zenon", 38, "435688");
+        Student ania = new Student("Anna", 40, "564534");
+        Student zenek = new Student("Zenon", 28, "435688");
         return Stream.of(pawel, robert, monika, robert1, ania, zenek);
     }
 
