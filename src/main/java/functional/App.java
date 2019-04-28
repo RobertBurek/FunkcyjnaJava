5bf5bf8f2ccd73547ba1d4b347e599e9c063bbbf
Robert Burek <robertburek@wp.pl>
2019-04-28 16:09:05 +0200

    I cleaned App

--- src/main/java/functional/App.java
+++ src/main/java/functional/App.java
@@ -1,6 +1,5 @@
 package functional;
 
-import java.util.Comparator;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -16,58 +15,12 @@ public class App {
 
         Predicate<Student> over30 = student -> (student.getAge() > 30);
         Consumer<Object> print = System.out::println;
+        Consumer<Student> printAll = student -> System.out.println(student.getName() + ", wiek: " + student.getAge());
         Function<Student, String> getStudentName = Student::getName;
         Function<Student, Integer> getStudentAge = Student::getAge;
 
-        System.out.println("Imiona wszystkich studentów ");//alfabetycznie: ");
-        createDataStream()
-                .map(getStudentName)
-                .forEach(print);
-
-        //limit
-        System.out.println("Pierwsze trzy (limit) imiona studentów alfabetycznie: ");
-        createDataStream().limit(3).map(getStudentName).sorted().forEach(print);
-
-        //skip
-        System.out.println("Wszystkie pozostałe poza czterema pierwszymi (skip) alfabetycznie: ");
-        createDataStream().skip(4).map(getStudentName).sorted().forEach(print);
-
-        //distinct
-        System.out.println("Wszystkie imiona o unikalnym indeksie (distinct) wymaga equals(Object o): ");
-        createDataStream().distinct().map(getStudentName).forEach(print);
-
-        //sorted
-        System.out.println("Wszystkie imiona alfabetycznie (sorted) listy stringów (imion): ");
-        createDataStream().map(getStudentName).sorted().forEach(print);
-
-        //sorted
-        System.out.println("Imiona alfabetycznie (sorted) streama wymaga implementacji Comparable<Student>: ");
-        createDataStream().sorted().map(getStudentName).forEach(print);
-
-        //sorted
-        System.out.println("Imiona posortowane po wieku-rosnąco (sorted(Comparator()-pełny zapis) w streamie: ");
-        createDataStream().sorted(new Comparator<Student>() {
-            @Override
-            public int compare(Student o1, Student o2) {
-                return Integer.compare(o1.getAge(), o2.getAge());
-            }
-        }).map(getStudentName).forEach(print);
-
-        //sorted
-        System.out.println("Imiona posortowane po wieku-malejąco (sorted(Comparator()-lambda) w streamie: ");
-        createDataStream().sorted((o1, o2) -> Integer.compare(o2.getAge(), o1.getAge())).map(getStudentName).forEach(print);
-
-        //sorted
-        System.out.println("Imiona posortowane po wieku-rosnąco (sorted(Comparator().comparingint()-referencja): ");
-        createDataStream().sorted(Comparator.comparing(Student::getAge)).map(getStudentName).forEach(print);
-
-        //count
-        System.out.print("Ilość elementów w streami (count): ");
-        long countAgeStudents = createDataStream()
-                .sorted(Comparator.comparing(Student::getAge))
-                .map(getStudentName)
-                .count();
-        System.out.println(countAgeStudents);
+        System.out.println("Lista studentów i ich wiek: ");
+        createDataStream().forEach(printAll);
 
     }
 
